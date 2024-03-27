9ed3eee46a90da13440ebca9c0accb795ad96953
Derek <sego37@gmail.com>
2024-03-26 18:30:50 -0600

    inicial commit

---
+++ assets/js/blog.js
@@ -0,0 +1,19 @@
+const username = document.getElementById('username')
+const title = document.getElementById('title')
+const comment = document.getElementById('comment')
+const submitbt = document.getElementById('submitbt')
+
+const post = document.createElement(commentsec);
+
+const blogtemp = `
+<div>
+            <h2 class="title">
+${.title}
+            </h2>
+            <p class="thoughts">
+${.thoughts}
+            </p>
+            <footer class="name">
+${.name}
+            </footer>
+        </div>`;
---
+++ assets/js/form.js
@@ -0,0 +1,33 @@
+const usernameEl = document.getElementById('username')
+const titleEl = document.getElementById('title')
+const commentEl = document.getElementById('comment')
+const submitbtEl = document.getElementById('submitbt')
+
+
+
+
+if (!localStorage.getItem("blogPosts")) {
+    localStorage.setItem('blogPosts', JSON.stringify([]))
+}
+
+
+submitbt.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let blogPosts = JSON.parse(localStorage.getItem("blogPosts"))
+
+    const username = usernameEl.value
+    const title = titleEl.value 
+    const comment = commentEl.value
+
+    const blogPostObj = {
+        username,
+        title,
+        comment,
+    }
+    
+    blogPosts.push(blogPostObj)
+
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts))
+ window.location.href = "blog.html"
+})
